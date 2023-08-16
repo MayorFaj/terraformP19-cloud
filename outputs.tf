5d6dd9496166dc729a853c92ec65ba4a926d813d
Mayor <mayorfaj@gmail.com>
2023-08-16 23:25:29 +0100

    output

--- outputs.tf
+++ outputs.tf
@@ -11,12 +11,3 @@ output "dynamodb_table_name" {
   value       = aws_dynamodb_table.terraform_locks.name
   description = "The name of the DynamoDB table"
 }
-
-
-output "alb_target_group_arn" {
-  value       = module.alb.alb_target_group_arn
-}
-
-output "alb_dns_name" {
-  value       = module.alb.alb_dns_name
-}
