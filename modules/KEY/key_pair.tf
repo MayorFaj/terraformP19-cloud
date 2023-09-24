43db97a5eb66405eea55606d64f6c75be67538b1
Mayor <mayorfaj@gmail.com>
2023-09-24 14:15:27 +0100

    Changes made

--- modules/KEY/key_pair.tf
+++ modules/KEY/key_pair.tf
@@ -2,5 +2,5 @@
 
 resource "aws_key_pair" "terraform-pbl19" {
   key_name   = "pbl19-key"
-  public_key = file("../modules/autoscaling/pbl19-key.pub")
+  public_key = file("../modules/KEY/pbl19-key.pub")
 }
