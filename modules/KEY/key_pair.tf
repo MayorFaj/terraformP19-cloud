a8b38347527a580e239e22203a60e6d5c1ee1421
Mayor <mayorfaj@gmail.com>
2023-09-24 16:33:18 +0100

    changes made

--- modules/KEY/key_pair.tf
+++ modules/KEY/key_pair.tf
@@ -1,11 +1,15 @@
 #----KEY-keypair.tf
 
-data "external" "public_key" {
-  program = ["bash", "-c", "cat ~/.ssh/pbl19-key.pub"]
+# data "external" "public_key" {
+#   program = ["bash", "-c", "cat ~/.ssh/pbl19-key.pub"]
+# }
+
+variable "private_key_path" {
+  default = "~/.ssh/MyKeyPairName.pem"
 }
 
 resource "aws_key_pair" "terraform-pbl19" {
   key_name   = "pbl19-key"
-  public_key = data.external.public_key.result
+  public_key = file(var.private_key_path)
   ##file("~/.ssh/pbl19-key.pub")
 }
